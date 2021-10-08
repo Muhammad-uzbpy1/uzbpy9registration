c69aa13efc3d8c0316dc5b26e5512c50fd1225ee
Muhammad-uzbpy1 <92166979+Muhammad-uzbpy1@users.noreply.github.com>
2021-10-08 16:57:13 +0300

    yukladim

---
+++ script.js
@@ -0,0 +1,8 @@
+function login() {
+    var ism=document.getElementById('ism').value;
+    var familiya=document.getElementById('familiya').value;
+    var email=document.getElementById('email').value;
+    var parol=document.getElementById('parol').value;
+    document.write(ism + familiya + email + parol)
+
+}
